b2d6123239bc2ad416cf41e49cd9723db42c258b
kiplangatwayne <waynekiplangatkorir903@gmail.com>
2023-06-13 11:34:21 +0300

    completed  assingment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, + ${Grace Hopper}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+currentUser.slice(0, 1);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
